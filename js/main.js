c322059e710eec70561ee82a34da6590a71c417b
mlgmontage <kettleofrassel1@gmail.com>
2020-05-15 05:38:03 -0500

    rating stats

--- js/main.js
+++ js/main.js
@@ -303,3 +303,21 @@ jQuery(document).ready(function ($) {
       counter();
     });
 });
+
+// chart
+// const ctx = document.getElementById("rating").getContext("2d");
+
+// const chart = new Chart(ctx, {
+//   type: "pie",
+//   data: {
+//     labels: ["Лайк", "Дизлайк"],
+//     datasets: [
+//       {
+//         label: "Рейтинг",
+//         data: [80, 20],
+//         backgroundColor: ["rgba(255, 0, 0, 1)", "rgba(0, 0, 255, 1)"],
+//         borderWidth: 0,
+//       },
+//     ],
+//   },
+// });
